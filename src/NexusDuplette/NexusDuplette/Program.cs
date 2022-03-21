15eca12ad5f6928730935af2656222b6b1e3fa75
Kaltenbach, Robin <robin@cskaltenbach.de>
2022-03-21 13:09:06 +0100

    Configuration of App

--- src/NexusDuplette/NexusDuplette/Program.cs
+++ src/NexusDuplette/NexusDuplette/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
 Console.WriteLine("Hello, World!");
+
+
+var logger = LogManager.GetCurrentClassLogger();
+
+try
+{
+    var config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory()) //From NuGet Package Microsoft.Extensions.Configuration.Json
+        .AddJsonFile("appsettings.json", true, true)
+        .Build();
+
+    await using var servicesProvider = new ServiceCollection()
+        .AddLogging(loggingBuilder =>
+        {
+            // configure Logging with NLog
+            loggingBuilder.ClearProviders();
+            loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+            loggingBuilder.AddNLog(config);
+        }).BuildServiceProvider();
+
+
+    logger.Info("# # # # Start von HomeOfficeManagement # # # #");
+    logger.Info("Logger initialisiert");
+}
+catch (Exception ex)
+{
+    // NLog: catch any exception and log it.
+    logger.Error(ex, "Stopped program because of exception");
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    LogManager.Shutdown();
+}
