2e52eb796614c957cf691ab44910a28b5944cde9
Robin Kaltenbach <robin@cskaltenbach.de>
2025-06-26 21:03:49 +0200

    test

--- src/NexusDuplette/NexusDuplette.Core/DuplettenFinder.cs
+++ src/NexusDuplette/NexusDuplette.Core/DuplettenFinder.cs
@@ -1,14 +1,31 @@
-﻿namespace NexusDuplette.Core;
+﻿using NLog;
+
+namespace NexusDuplette.Core;
 
 public class DuplettenFinder : IDuplettenFinder
 {
+    private readonly ILogger _logger;
     public DuplettenFinder()
     {
-        
+        _logger = LogManager.GetCurrentClassLogger();
     }
 
     public bool StartDuplettenMatching(IEnumerable<string> pathsToAnalyze)
     {
-        throw new NotImplementedException();
+        var gesamtDateiliste = new List<string>();
+        _logger.Debug("Sammel alle Filenamen in Gesamtliste");
+        foreach (var pfad in pathsToAnalyze)
+        {
+            _logger.Debug("Gehe in Pfad {path}", pfad);
+            gesamtDateiliste.AddRange(GetAllFilesWithoutHierarchy(pfad));
+        }
+
+        return true;
+    }
+
+    private IEnumerable<string> GetAllFilesWithoutHierarchy(string path)
+    {
+        FileInfo i = new FileInfo(path);
+        return null;
     }
 }
--- src/NexusDuplette/NexusDuplette/Program.cs
+++ src/NexusDuplette/NexusDuplette/Program.cs
@@ -2,11 +2,12 @@
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NexusDuplette.Core;
 using NLog;
 using NLog.Extensions.Logging;
-
-Console.WriteLine("Hello, World!");
+using ILogger = NLog.ILogger;
 
 
 var logger = LogManager.GetCurrentClassLogger();
@@ -15,10 +16,20 @@ try
 {
     var config = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory()) //From NuGet Package Microsoft.Extensions.Configuration.Json
+        .AddEnvironmentVariables()
         .AddJsonFile("appsettings.json", true, true)
         .Build();
 
+    if (Environment.CurrentDirectory.Contains("bin"))
+    {
+        
+    }
+
+    var allowedPaths = config.GetRequiredSection("allowedPaths").Get<IEnumerable<string>>().ToList();
+
     await using var servicesProvider = new ServiceCollection()
+        .AddScoped<IDuplettenFinder, DuplettenFinder>()
+        .AddSingleton<ILogger, Logger>()
         .AddLogging(loggingBuilder =>
         {
             // configure Logging with NLog
@@ -27,9 +38,26 @@ try
             loggingBuilder.AddNLog(config);
         }).BuildServiceProvider();
 
+    logger.Info("# # # # Start von DuplettenFinder # # # #");
+    logger.Debug("Logger initialisiert");
 
-    logger.Info("# # # # Start von HomeOfficeManagement # # # #");
-    logger.Info("Logger initialisiert");
+    var service = servicesProvider.GetRequiredService<IDuplettenFinder>();
+    logger.Debug("Service {service} initialisiert", service.GetType());
+    logger.Info("Starte Duplettensuche");
+    
+
+    if (allowedPaths.Count < 1)
+    {
+        logger.Warn("Alles erledigt, es wurden keine Pfade angegeben");
+        
+    }
+    else
+    {
+        logger.Debug("Es wurden {anzahl} Pfade zum Analysieren angegeben", allowedPaths.Count);
+        service.StartDuplettenMatching(allowedPaths);
+    }
+
+    logger.Info("Beende Duplettenfinder");
 }
 catch (Exception ex)
 {
